40b11eb6d2f75bd96b00f0448bad9d3729f561fa
Spencer Rhoades <spencerrhoades@205-185-96-237.directcom.com>
2020-01-18 16:47:16 -0700

    add link to landing page

--- web/index.php
+++ web/index.php
@@ -15,11 +15,11 @@
       <h2>About me</h2>
     </a>
     <a class="picLink" href="w02/assignments.html">
-      <img src="images/family.png" alt="My family">
+      <img src="images/working.jpg" alt="Man working at computer">
       <h2>Assignments</h2>
     </a>
     <a class="picLink" href="project/index.html">
-      <img src="images/family.png" alt="My family">
+      <img src="images/books.jpg" alt="Stack of books">
       <h2>School of the Prophets</h2>
     </a>
   </div>
