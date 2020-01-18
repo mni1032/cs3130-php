a2a70ef0111921dbc54142717f0e1dccd4eab9f7
Spencer Rhoades <spencerrhoades@205-185-96-237.directcom.com>
2020-01-18 11:24:44 -0700

    add color to header

--- web/index.php
+++ web/index.php
@@ -1,5 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>CS 313</title>
+</head>
 
-  include "header.php";
-
-?>
+<body>
+  <?php
+    include "header.php";
+  ?>
+</body>
+</html>
