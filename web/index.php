7ab906d4228a0017a7d49d5cf6b218a505d83ffe
Spencer Rhoades <spencerrhoades@205-185-96-237.directcom.com>
2020-01-18 22:25:52 -0700

    add link to landing page

--- web/index.php
+++ web/index.php
@@ -11,18 +11,24 @@
 <body>
   <?php include "header.php";?>
   <div id="landingMenu" class="flex">
-    <a class="picLink" href="w02/prove/aboutMe.php" onmouseover="changeTextColor(this)">
+    <a class="picLink" href="w02/prove/aboutMe.php">
       <img src="images/family.png" alt="My family">
       <h2>About me</h2>
     </a>
-    <a class="picLink" href="w02/prove/assignments.html" onmouseover="changeTextColor(this)">
+    <a class="picLink" href="w02/prove/assignments.html">
       <img src="images/working.jpg" alt="Man working at computer">
       <h2>Assignments</h2>
     </a>
-    <a class="picLink" href="project/index.html" onmouseover="changeTextColor(this)">
+    <a class="picLink" href="project/index.html">
       <img src="images/books.jpg" alt="Stack of books">
       <h2>School of the Prophets</h2>
     </a>
   </div>
+  <script>
+    var links = document.getElementsByClassName('picLink');
+    links[0].onmouseover = changeTextColor(links[0].id);
+    links[1].onmouseover = changeTextColor(links[1].id);
+    links[2].onmouseover = changeTextColor(links[2].id);
+  </script>
 </body>
 </html>
