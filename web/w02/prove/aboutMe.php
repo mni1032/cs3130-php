331c5a7972b62f7385af6a91716ad8fb464c7b20
Spencer Rhoades <spencerrhoades@205-185-96-237.directcom.com>
2020-01-18 19:59:38 -0700

    add link to landing page

--- web/w02/prove/aboutMe.php
+++ web/w02/prove/aboutMe.php
@@ -12,8 +12,14 @@
     <div id="familyPicHolder">
         <img id="familyPic" src="../../images/family.png">
     </div>
-    <p class="aboutInfo">My name is Spencer Rhoades. I'm from Fort Worth, Texas, but I love the cold! My major is computer science, but I'm planning 
+    <p id="me" class="aboutInfo">My name is Spencer Rhoades. I'm from Fort Worth, Texas, but I love the cold! My major is computer science, but I'm planning 
         to be a seminary teacher. I love reading, coding, hiking, and pretty much every other hobby.
+    </p>
+    <p id="rebecca" class="aboutInfo">This is my wife, Rebecca. We met up here at BYU-I and have been married for about a year and a half.
+        She's from Lawton, Oklahoma and enjoys baking, making string art, and animals!
+    </p>
+    <p id="paul-allen" class="aboutInfo">Paul-Allen is our seven-month-old son. He's much bigger thn this picture now and can crawl,
+        pull himself up to standing, laugh, growl, and wave. He also likes jumping and eating real food.
     </p>
 </body>
 </html>
