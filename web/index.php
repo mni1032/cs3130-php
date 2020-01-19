a5c5aa0b30192cb07937d20e8a2bdb4be9e2440e
Spencer Rhoades <spencerrhoades@205-185-96-237.directcom.com>
2020-01-18 22:39:06 -0700

    add link to landing page

--- web/index.php
+++ web/index.php
@@ -11,15 +11,15 @@
 <body>
   <?php include "header.php";?>
   <div id="landingMenu" class="flex">
-    <a class="picLink" href="w02/prove/aboutMe.php" onmouseover="changeTextColor(this.id);">
+    <a id="about" class="picLink" href="w02/prove/aboutMe.php" onmouseover="changeTextColor(this.id);">
       <img src="images/family.png" alt="My family">
       <h2>About me</h2>
     </a>
-    <a class="picLink" href="w02/prove/assignments.html">
+    <a id="assign" class="picLink" href="w02/prove/assignments.html">
       <img src="images/working.jpg" alt="Man working at computer">
       <h2>Assignments</h2>
     </a>
-    <a class="picLink" href="project/index.html">
+    <a id="project" class="picLink" href="project/index.html">
       <img src="images/books.jpg" alt="Stack of books">
       <h2>School of the Prophets</h2>
     </a>
