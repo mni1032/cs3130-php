ca892fde8c9d99ee35cca3768add9f7d4e98b351
Spencer Rhoades <spencerrhoades@205-185-96-237.directcom.com>
2020-01-18 21:08:19 -0700

    add link to landing page

--- web/w02/prove/aboutMe.php
+++ web/w02/prove/aboutMe.php
@@ -11,18 +11,18 @@
     <?php include "../../header.php";?>
     <div id="familyPicHolder">
         <img id="familyPic" src="../../images/family.png">
-        <span id="meBio" class="aboutInfo me">My name is Spencer Rhoades. I'm from Fort Worth, Texas, but I love the cold! My major is computer science, but I'm planning 
+        <span id="meBio" class="aboutInf">My name is Spencer Rhoades. I'm from Fort Worth, Texas, but I love the cold! My major is computer science, but I'm planning 
             to be a seminary teacher. I love reading, coding, hiking, and pretty much every other hobby.
         </span>
-        <span id="rebeccaBio" class="aboutInfo rebecca">This is my wife, Rebecca. We met up here at BYU-I and have been married for about a year and a half.
+        <span id="rebeccaBio" class="aboutInfo">This is my wife, Rebecca. We met up here at BYU-I and have been married for about a year and a half.
             She's from Lawton, Oklahoma and enjoys baking, making string art, and animals!
         </span>
-        <span id="paul-allenBio" class="aboutInfo paul-allen">Paul-Allen is our seven-month-old son. He's much bigger thn this picture now and can crawl,
+        <span id="paul-allenBio" class="aboutInfo">Paul-Allen is our seven-month-old son. He's much bigger thn this picture now and can crawl,
             pull himself up to standing, laugh, growl, and wave. He also likes jumping and eating real food.
         </span>
-        <div id="me" class="faceBox me"></div>
-        <div id="rebecca" class="faceBox rebecca"></div>
-        <div id="paul-allen" class="faceBox paul-allen"></div>
+        <div id="myFace" class="faceBox"></div>
+        <div id="rebeccaFace" class="faceBox"></div>
+        <div id="paul-allenFace" class="faceBox"></div>
     </div>
 </body>
 </html>
