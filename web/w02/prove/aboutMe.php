04bdc5d690df765c8e34cd8a7131eb091b8953e2
Spencer Rhoades <spencerrhoades@205-185-96-237.directcom.com>
2020-01-18 20:06:02 -0700

    add link to landing page

--- web/w02/prove/aboutMe.php
+++ web/w02/prove/aboutMe.php
@@ -11,15 +11,15 @@
     <?php include "../../header.php";?>
     <div id="familyPicHolder">
         <img id="familyPic" src="../../images/family.png">
-        <p id="me" class="aboutInfo">My name is Spencer Rhoades. I'm from Fort Worth, Texas, but I love the cold! My major is computer science, but I'm planning 
+        <span id="me" class="aboutInfo">My name is Spencer Rhoades. I'm from Fort Worth, Texas, but I love the cold! My major is computer science, but I'm planning 
             to be a seminary teacher. I love reading, coding, hiking, and pretty much every other hobby.
-        </p>
-        <p id="rebecca" class="aboutInfo">This is my wife, Rebecca. We met up here at BYU-I and have been married for about a year and a half.
+        </span>
+        <span id="rebecca" class="aboutInfo">This is my wife, Rebecca. We met up here at BYU-I and have been married for about a year and a half.
             She's from Lawton, Oklahoma and enjoys baking, making string art, and animals!
-        </p>
-        <p id="paul-allen" class="aboutInfo">Paul-Allen is our seven-month-old son. He's much bigger thn this picture now and can crawl,
+        </span>
+        <span id="paul-allen" class="aboutInfo">Paul-Allen is our seven-month-old son. He's much bigger thn this picture now and can crawl,
             pull himself up to standing, laugh, growl, and wave. He also likes jumping and eating real food.
-        </p>
-        </div>
+        </span>
+    </div>
 </body>
 </html>
