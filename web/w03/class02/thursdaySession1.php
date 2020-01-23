bb9454d5b881c8328eb89d84dc18faaeb303a6fd
Spencer Rhoades <spencerrhoades@Spencers-MacBook-Air.local>
2020-01-23 16:08:53 -0700

    cookie class activity

--- web/w03/class02/thursdaySession1.php
+++ web/w03/class02/thursdaySession1.php
@@ -13,6 +13,11 @@
          echo "Session variables have been set.<br/>";?>
       <a href="thursdaySession2.php">Check the variables on another page</a>
 
+      <h3>Let's do it with a form.</h3>
+      <form action="" method="POST">
+         <input type="text" name="picture"/>
+         <input type="submit" name="Submit" value="Submit!"/>
+      </form?
       <?php // set session variables using a form ?>
    </body>
 </html>
