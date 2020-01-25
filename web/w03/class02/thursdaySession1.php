aaecc696e69fcb2ebef54a78526653aa701547e5
Spencer Rhoades <spencerrhoades@205-185-96-237.directcom.com>
2020-01-25 13:03:30 -0700

    rename CS 313 to CS313 and start prove03

--- web/w03/class02/thursdaySession1.php
+++ web/w03/class02/thursdaySession1.php
@@ -14,7 +14,7 @@
       <a href="thursdaySession2.php">Check the variables on another page</a>
 
       <h3>Let's do it with a form.</h3>
-      <form action="" method="POST"/>
+      <form action="" method="POST">
          <input type="text" name="picture"/>
          <input type="submit" name="Submit" value="Submit!"/>
       </form>
