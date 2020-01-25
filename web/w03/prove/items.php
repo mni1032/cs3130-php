9ec85e5ff9c5ab8b2ec7f04e39b5026c896dbc06
Spencer Rhoades <spencerrhoades@205-185-96-237.directcom.com>
2020-01-25 14:32:08 -0700

    finish browse page

--- web/w03/prove/items.php
+++ web/w03/prove/items.php
@@ -7,6 +7,6 @@
         "pen" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJv9OzNtdK3_p8syebTdgxx4MiEpQSlBwvoJRD4GzWp8kNcmSV&s",
         "backpack" => "https://cdn-us-ec.yottaa.net/54636bb786305e35ea00040e/ca3ea000f77501321219123dfe2baf36.yottaa.net/v~4b.647/is/image/im9/377679_1_1?resmode=4&op_usm=1,1,1,&qlt=70,1&hei=600&wid=600&align=0,1&yocs=3A_3B_",
         "laptop" => "https://mondrian.mashable.com/uploads%252Fcard%252Fimage%252F833990%252Fc89e05ec-e622-4e06-960d-f477fa751715.jpeg%252F480x0.jpeg?signature=sqgvP8HyRkslD1a9R8OVS1VfPYA=&source=https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com",
-        "microsope" => "https://mediclinic.qodeinteractive.com/wp-content/uploads/2016/09/product5-582x805.jpg"
+        "microsope" => "http://www.liberaldictionary.com/wp-content/uploads/2018/12/microscope.jpg"
     )
 ?>
