da0841544796421847affb8079c94e2352717682
Spencer Rhoades <spencerrhoades@Spencers-MacBook-Air.local>
2020-02-06 16:29:20 -0700

    team activity 2

---
+++ web/w05/team02/index.php
@@ -0,0 +1,14 @@
+<?php 
+    require "../class01/dbConnect.php";
+    $db = get_db();
+
+    $events = $db->prepare("SELECT name, image FROM events");
+    $events->execute();
+
+    while ($eRow = $events->fetch(PDO::FETCH_ASSOC)) {
+        $event_name = $eRow["name"];
+        $event_img = $eRow["image"];
+
+        echo "<div><p>$event_name</p><img src=\"$event_img\"/></div>";
+    }
+?>
