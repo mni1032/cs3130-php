c4234212469ca19bddb58d0168e350ca7ed19ac4
Spencer Rhoades <spencerrhoades@205-185-96-237.directcom.com>
2020-02-08 18:40:51 -0700

    add styles sheet href to project

--- web/project/commentary.php
+++ web/project/commentary.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="styles.css">
     <title>School of the Prophets</title>
 </head>
 
 <body>
     <?php
         include "header.php";
-        
+
         if (isset($_POST['submit'])) {
 
             $book = $_POST['book'];
