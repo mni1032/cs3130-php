896db954fd357d0fbb96681f73a9d5b35b0c3476
Spencer Rhoades <spencerrhoades@Spencers-MacBook-Air.local>
2020-02-11 15:44:35 -0700

    begin w6 activity

--- web/w06/class01/display.php
+++ web/w06/class01/display.php
@@ -5,8 +5,24 @@
 	<body>
 		<div class="container">
          <?php
-            // retrieve url parameter
-            // execute query to pull up data from that id
+            $personId = $_GET['personId'];
+            
+            $statement = $db->prepare('SELECT * FROM w6_user WHERE id = :personId;');
+            $statement->bindValue(':personId', $personId);
+            $statement->execute();
+            while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+               $id = $row['id'];
+               $first = $row['first_name'];
+               $last = $row['last_name'];
+               $food_id = $row['food_type'];
+
+               $statement = $db->prepare('SELECT food FROM w6_food WHERE id = $food_id;');
+               $statement->execute();
+               while ($fRow = $statement->fetch(PDO::FETCH_ASSOC)) {
+                  $food = $fRow['food'];
+               }
+               echo "<h1>$first $last's favorite food is $food.</h1>"
+            }
             // execute another query to get food data
             // display name and favorite food
          ?>
