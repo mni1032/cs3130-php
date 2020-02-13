62194f44fd6e605b28723c8413272e4e65030876
Spencer Rhoades <spencerrhoades@205-185-96-227.directcom.com>
2020-02-12 19:26:14 -0700

    team activity

--- web/w06/team01/display.php
+++ web/w06/team01/display.php
@@ -5,8 +5,6 @@
 	<body>
 		<div class="container">
          <?php
-            $lastInsert = $_GET['lastInsert'];
-
             $statement = $db->prepare("SELECT s.id, s.book, s.chapter, s.verse, string_agg(t.name, ', ') AS topics FROM Scriptures s INNER JOIN scripture_topic st ON s.id = st.scripture_id INNER JOIN topic t ON st.topic_id = t.id GROUP BY s.id;");
             $statement->execute();
             while ($sRow = $statement->fetch(PDO::FETCH_ASSOC)) {
