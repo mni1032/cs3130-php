2c11bb507b4e4508f371ab93570dbe36c4a6649f
Spencer Rhoades <spencerrhoades@205-185-96-227.directcom.com>
2020-02-12 18:57:06 -0700

    team activity

---
+++ web/w06/team01/display.php
@@ -0,0 +1,30 @@
+<?php
+	require("dbConnect.php");
+	$db = get_db();
+?>
+	<body>
+		<div class="container">
+         <?php
+            $lastInsert = $_GET['lastInsert'];
+            echo "<p>$lastInsert</p>";
+            // $statement = $db->prepare('SELECT * FROM w6_user WHERE id = :personId;');
+            // $statement->bindValue(':personId', $personId);
+            // $statement->execute();
+            // while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+            //    $id = $row['id'];
+            //    $first = $row['first_name'];
+            //    $last = $row['last_name'];
+            //    $food_id = $row['food_type'];
+
+            //    $foodStatement = $db->prepare("SELECT food FROM w6_food WHERE id = $food_id;");
+            //    $foodStatement->execute();
+            //    while ($fRow = $foodStatement->fetch(PDO::FETCH_ASSOC)) {
+            //       $food = $fRow['food'];
+            //    }
+            //    echo "<h1>$first $last's favorite food is $food.</h1>";
+            // }
+         ?>
+
+		</div>
+	</body>
+</html>
--- web/w06/team01/insert.php
+++ web/w06/team01/insert.php
@@ -6,39 +6,38 @@ $verse = $_POST['verse'];
 $content = $_POST['content'];
 $topics = $_POST['topics'];
 
-echo "<h1>$book $chapter:$verse</h1>";
-echo "<p>$content</p>";
-foreach ($topics as $topic) {
-    echo "<p>$topic</p>";
-} 
+require("dbConnect.php");
+$db = get_db();
 
-// require("dbConnect.php");
-// $db = get_db();
+try
+{
+	// insert into database
+	$query = 'INSERT INTO Scriptures (book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content);';
+	$statement = $db->prepare($query);
+	$statement->bindValue(':book', $book);
+	$statement->bindValue(':chapter', $chapter);
+    $statement->bindValue(':verse', $verse);
+    $statement->bindValue(':content', $content);
+    $statement->execute();
 
-// try
-// {
-// 	// insert into database
-// 	$query = 'INSERT INTO Scriptures (book, chapter, verse) VALUES (:book, :chapter, :verse, :content)';
-// 	$statement = $db->prepare($query);
-// 	$statement->bindValue(':book', $book);
-// 	$statement->bindValue(':chapter', $chapter);
-//     $statement->bindValue(':verse', $verse);
-//     $statement->bindValue(':content', $content);
-//     $statement->execute();
-
-//     $scriptureId = $db->lastInsertId("scriptures_id_seq");
+    $scriptureId = $db->lastInsertId("scriptures_id_seq");
     
-//     foreach ($topics as $topic) {
+    foreach ($topics as $topic) {
+        $query = 'INSERT INTO scripture_topic (scripture_id, topic_id) VALUES (:scriptureId, :topicId);';
+        $statement = $db->prepare($query);
+        $statement->bindValue(':scriptureId', $scriptureId);
+        $statement->bindValue(':topicId', $topic);
+        $statement->execute();
+    }
 
-//     }
-//     $query = 'INSERT INTO scripture_topic (scripture_id, topic_id) VALUES (:scriptureId'
-// }
-// catch (Exception $ex)
-// {
-// 	echo "Error with DB. Details: $ex";
-// 	die();
-// }
-// header("Location: display.php");
+    $topicId = $db->lastInsertId("scripture_topic_id_seq");
+}
+catch (Exception $ex)
+{
+	echo "Error with DB. Details: $ex";
+	die();
+}
+header("Location: display.php/?lastInsert=$topicId");
 
-// die(); 
+die(); 
 ?>
