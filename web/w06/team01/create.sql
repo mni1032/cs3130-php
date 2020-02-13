6e7ff1d0921858887127b5d5021a96eff6dcc5e5
Spencer Rhoades <spencerrhoades@205-185-96-227.directcom.com>
2020-02-12 18:45:27 -0700

    team activity

---
+++ web/w06/team01/create.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS scripture_topic;
+DROP TABLE IF EXISTS topic;
+DROP TABLE IF EXISTS Scriptures;
+
+CREATE TABLE Scriptures (
+  id SERIAL NOT NULL PRIMARY KEY,
+  book VARCHAR(100) NOT NULL,
+  chapter INT NOT NULL,
+  verse INT NOT NULL,
+  content VARCHAR(400) NOT NULL
+);
+
+CREATE TABLE topic (
+  id SERIAL NOT NULL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE scripture_topic (
+  id SERIAL NOT NULL PRIMARY KEY,
+  scripture_id INT NOT NULL REFERENCES Scriptures(id),
+  topic_id INT NOT NULL REFERENCES topic(id)
+);
+
+INSERT INTO topic (name)
+VALUES ('faith');
+INSERT INTO topic (name)
+VALUES ('sacrifice');
+INSERT INTO topic (name)
+VALUES ('charity');
