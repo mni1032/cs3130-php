15cada588ec5718bbc430bed28f72517786645e3
Spencer Rhoades <spencerrhoades@205-185-96-227.directcom.com>
2020-02-15 19:38:37 -0700

    add verse adding page

--- web/project/insertVerse.php
+++ web/project/insertVerse.php
@@ -4,30 +4,25 @@
     $verse = intval($_POST['verse']);
     $text = $_POST['text'];
 
-    var_dump($book);
-    var_dump($chapter);
-    var_dump($verse);
-    var_dump($text);
+    require("dbConnect.php");
+    $db = get_db();
 
-    // require("dbConnect.php");
-    // $db = get_db();
+    try {
+        $query = 'INSERT INTO verse (book, chapter, verse, text) VALUES (:book, :chapter, :verse, :text);';
+        $statement = $db->prepare($query);
+        $statement->bindValue(':book', $book);
+        $statement->bindValue(':chapter', $chapter);
+        $statement->bindValue(':verse', $verse);
+        $statement->bindValue(':text', $text);
 
-    // try {
-    //     $query = 'INSERT INTO verse (book, chapter, verse, text) VALUES (:book, :chapter, :verse, :text);';
-    //     $statement = $db->prepare($query);
-    //     $statement->bindValue(':book', $book);
-    //     $statement->bindValue(':chapter', $chapter);
-    //     $statement->bindValue(':verse', $verse);
-    //     $statement->bindValue(':text', $text);
+        $lastVerse = $db->lastInsertId("verse_id_seq");
+    }
+    catch (Exception $ex)
+    {
+        echo "Error with DB. Details: $ex";
+        die();
+    }
 
-    //     $lastVerse = $db->lastInsertId("verse_id_seq");
-    // }
-    // catch (Exception $ex)
-    // {
-    //     echo "Error with DB. Details: $ex";
-    //     die();
-    // }
-
-    // header("Location: addVerse.php?lastVerse=$lastVerse");
-    // die(); 
+    header("Location: addVerse.php?lastVerse=$lastVerse");
+    die(); 
 ?>
