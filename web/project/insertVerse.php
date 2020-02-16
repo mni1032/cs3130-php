191227fa3079e64bb421db860da5443be1040c5c
Spencer Rhoades <spencerrhoades@205-185-96-227.directcom.com>
2020-02-15 19:20:38 -0700

    add verse adding page

---
+++ web/project/insertVerse.php
@@ -0,0 +1,28 @@
+<?php
+    $book = $_POST['book'];
+    $chapter = $_POST['chapter'];
+    $verse = $_POST['verse'];
+    $text = $_POST['text'];
+
+    require("dbConnect.php");
+    $db = get_db();
+
+    try {
+        $query = 'INSERT INTO verse (book, chapter, verse, text) VALUES (:book, :chapter, :verse, :text);';
+        $statement = $db->prepare($query);
+        $statement->bindValue(':book', $book);
+        $statement->bindValue(':chapter', $chapter);
+        $statement->bindValue(':verse', $verse);
+        $statement->bindValue(':text', $text);
+
+        
+    }
+    catch (Exception $ex)
+    {
+        echo "Error with DB. Details: $ex";
+        die();
+    }
+
+    header("Location: addVerse.php");
+    die(); 
+?>
