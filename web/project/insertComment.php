33c6f9d9d3100ba346bbb642e998f0d49d9250f9
Spencer Rhoades <spencerrhoades@205-185-96-227.directcom.com>
2020-02-15 20:26:05 -0700

    add comment adding page

--- web/project/insertComment.php
+++ web/project/insertComment.php
@@ -36,7 +36,7 @@
         $statement->bindValue(':citation', $citation);
         $statement->execute();
 
-        // $citationId = $db->lastInsert("citation_id_seq");
+        $citationId = $db->lastInsert("citation_id_seq");
 
         // $query = 'INSERT INTO comment (author_id, create_date, verse_id, citation_id, text) VALUES (:author_id, NOW(), :verse_id, :citation_id, :text);';
         // $statement = $db->prepare($query);
